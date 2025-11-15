8d756b520872913d8a8ade43435a952901fb74da
gugucazhiz <domateklh@gmail.com>
2025-11-15 00:00:25 -0300

    removed unused imports

--- teste_tela_enem/src/main/java/br/ufrn/tads/App.java
+++ teste_tela_enem/src/main/java/br/ufrn/tads/App.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import br.ufrn.tads.repository.imp.*;
 import java.io.IOException;
 
 /**
--- teste_tela_enem/src/main/java/br/ufrn/tads/repository/imp/DbConnection.java
+++ teste_tela_enem/src/main/java/br/ufrn/tads/repository/imp/DbConnection.java
@@ -3,7 +3,6 @@ package br.ufrn.tads.repository.imp;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
